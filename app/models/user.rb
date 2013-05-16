0a41036600fd6398e1ef84151a6479efb11b1123
Seliverstov Maxim <maxim.web.deveoper@gmail.com>
2013-05-16 07:40:22 +0000

    Modify user model. Add validators.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :password
+
+  has_secure_password
+
+  validates_presence_of :email
+  validates_uniqueness_of :email
+  validates_presence_of :email, :on => :create
 end
