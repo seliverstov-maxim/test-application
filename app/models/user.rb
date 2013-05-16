d0a7f65b358cc9c4603b65fe1435a43034e950f6
Seliverstov Maxim <maxim.web.developer@gmail.com>
2013-05-16 15:00:57 +0000

    Regenerate user. Add user CRUD.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,3 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :password
-
-  has_secure_password
-
-  validates_presence_of :email
-  validates_uniqueness_of :email
-  validates_presence_of :email, :on => :create
+  attr_accessible :email, :password_digest
 end
