e9c7333445fc9c6d9e17d75a0a8ee556bb8aef01
Seliverstov Maxim <maxim.web.deveoper@gmail.com>
2013-05-17 06:27:50 +0000

    Add authnticate method into user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation 
   has_secure_password 
   validates_presence_of :password, :on => :create
+
+  def self.authenticate(email, password)
+    find_by_email(email).try(:authenticate, password)
+  end
 end
