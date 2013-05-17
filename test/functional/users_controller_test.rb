0ee032484e4982505c0049dd11c7f0de6ff807a6
Seliverstov Maxim <maxim.web.deveoper@gmail.com>
2013-05-17 13:05:31 +0000

    Modify fixtures. Modify users_controller_test.

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
   setup do
-    @user = users(:one)
+    @user = users(:Bob)
   end
 
   test "should get index" do
@@ -18,7 +18,7 @@ class UsersControllerTest < ActionController::TestCase
 
   test "should create user" do
     assert_difference('User.count') do
-      post :create, user: { email: @user.email, password: @user.password }
+      post :create, user: { email: 'test@email', password: 'pass' }
     end
 
     assert_redirected_to user_path(assigns(:user))
