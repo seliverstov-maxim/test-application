b2ee77feae6eff1d1f2abb9ae54cb4a0bda02718
Seliverstov Maxim <maxim.web.developer@gmail.com>
2013-05-17 13:03:39 +0000

    Remove self.authenticate method.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,4 @@ class User < ActiveRecord::Base
   has_secure_password 
   validates_presence_of :password, :on => :create
 
-  def self.authenticate(email, password)
-    find_by_email(email).try(:authenticate, password)
-  end
 end
