4f0c633a054767ba671b0ffc5ed546d798aea68d
Seliverstov Maxim <maxim.web.developer@gmail.com>
2013-05-20 16:16:14 +0000

    Modify schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130516144603) do
+ActiveRecord::Schema.define(:version => 20130520155055) do
+
+  create_table "stories", :force => true do |t|
+    t.integer  "owner_id"
+    t.integer  "performer_id"
+    t.string   "state"
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email"
