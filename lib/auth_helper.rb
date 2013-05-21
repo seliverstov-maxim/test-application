33b16a1c8dced1929b0befd218196c87b639126c
Seliverstov Maxim <maxim.web.deveoper@gmail.com>
2013-05-21 06:33:35 +0000

    Implement AuthHelper.authenticate_user method.

--- lib/auth_helper.rb
+++ lib/auth_helper.rb
@@ -14,4 +14,10 @@ module AuthHelper
   def current_user
     @current_user ||= User.find_by_id(session[:user_id])
   end
+
+  def authenticate_user!
+    unless signed_in?
+      redirect_to new_session_path(from: request.url)
+    end
+  end
 end
