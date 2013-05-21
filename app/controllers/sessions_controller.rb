78c58a8ba1a4fe79e135b7261e68c1592b0e6a58
Seliverstov Maxim <maxim.web.deveoper@gmail.com>
2013-05-21 06:19:23 +0000

    Modify create and destroy actions of SessionController. Use single abstraction layer.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
+      sign_in(user);
       redirect_to root_path, notice: "You are successfully logged as #{user.email}"
     else
       flash.now[:alert] = "Invalid username or password"
@@ -14,7 +14,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session[:user_id] = nil
+    sign_out
     redirect_to root_url
   end
 end
