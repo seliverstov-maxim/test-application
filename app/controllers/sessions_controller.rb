44820fa563bfc0e00be4b626275bee7ca82bd233
Seliverstov Maxim <maxim.web.developer@gmail.com>
2013-05-21 06:35:54 +0000

    Modify skip_before_filter in SessionController.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   
-  skip_before_filter :require_login, :only => [:create]
+  skip_before_filter :authenticate_user!, :only => [:new, :create]
 
   def create
     user = User.find_by_email(params[:email])
