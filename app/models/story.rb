823ff4952e8c77e11dce7e1a1f24ebab69ccc6b4
Seliverstov Maxim <maxim.web.developer@gmail.com>
2013-05-23 20:32:44 +0400

    Modify attr_accessible.

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,5 +1,5 @@
 class Story < ActiveRecord::Base
-  attr_accessible :description, :title#, :owner_id, :performer_id, :state
+  attr_accessible :description, :title, :owner_id, :performer_id#, :state
 
   belongs_to :owner, class_name: "User"
   belongs_to :performer, class_name: "User"
