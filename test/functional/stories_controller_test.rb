cbb64b0a3e67bb976afcbb7c24b8b7aa52a9b7ed
Seliverstov Maxim <maxim.web.deveoper@gmail.com>
2013-05-23 20:40:07 +0400

    Add stub tests for StoriesController.

--- test/functional/stories_controller_test.rb
+++ test/functional/stories_controller_test.rb
@@ -2,8 +2,29 @@ require 'test_helper'
 
 class StoriesControllerTest < ActionController::TestCase
   setup do
-    @story = stories(:one)
+    @user = create :user
+    @story = create :story
+    sign_in @user
   end
 
-  
+  test 'should get index' do
+  end
+
+  test 'shoul get show' do
+  end
+
+  test 'should get new' do
+  end
+
+  test 'should get edit' do
+  end
+
+  test 'should crate story' do
+  end
+
+  test 'should update story' do
+  end
+
+  test 'should remove strory' do
+  end
 end
