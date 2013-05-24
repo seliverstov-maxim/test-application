178e6a442641741fdf65995112da687fabb3dd9f
Seliverstov Maxim <maxim.web.developer@gmail.com>
2013-05-24 11:25:58 +0400

    Add story_title, story_description sequences.

--- test/factories/sequences.rb
+++ test/factories/sequences.rb
@@ -2,4 +2,12 @@ FactoryGirl.define do
   sequence :email do |n|
     "email_#{n}@mail.com"
   end
+
+  sequence :story_title do |n|
+    "Story title #{n}"
+  end
+
+  sequence :story_description do |n|
+    "Story description #{n}"
+  end  
 end
