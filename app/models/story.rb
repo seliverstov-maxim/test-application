fa38f4b14cd219d0e9e90e22993cde1536b71fa6
Seliverstov Maxim <maxim.web.developer@gmail.com>
2013-05-29 18:19:29 +0400

    Implemet ransack filter.

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,4 +1,9 @@
 class Story < ActiveRecord::Base
+  scope :by_owner, ->(user_id) { where(owner_id: user_id) unless (user_id.blank?) }
+  scope :by_performer, ->(user_id) { where(performer_id: user_id) unless (user_id.blank?) }
+  scope :by_state, ->(user_id) { where(state: state) unless (state.blank?) }
+  scope :recent_order, -> { order('created_at desc') }
+
   attr_accessible :description, :title, :owner_id, :performer_id, :state_event#, :state
 
   belongs_to :owner, class_name: "User"
