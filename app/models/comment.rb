fc83ce89623425e034998f835a29d288e4157ba4
Seliverstov Maxim <maxim.web.deveoper@gmail.com>
2013-05-30 12:25:35 +0400

    Implement relations users, stories with comments.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ActiveRecord::Base
   attr_accessible :message, :owner_id, :story_id
+
+  belongs_to :owner, class_name: "User"
+  belongs_to :story
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ActiveRecord::Base
   validates_presence_of :password, :on => :create
 
   has_many :owner_stories, dependent: :destroy, class_name: "Story", foreign_key: "owner_id"
+  has_many :owner_comments, dependent: :destroy, class_name: "Comment", foreign_key: "owner_id"
   has_many :perfomer_stories, dependent: :nullify, class_name: "Story", foreign_key: "performer_id"
 end
