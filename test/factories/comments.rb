63b5ffec87705caf2c0fd1f5f99476af195fa52c
Seliverstov Maxim <maxim.web.developer@gmail.com>
2013-05-30 12:23:31 +0400

    Remove commentS factories.

--- test/factories/comments.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :comment do
-    owner_id 1
-    story_id 1
-    message "MyString"
-  end
-end
