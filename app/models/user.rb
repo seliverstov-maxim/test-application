550248a3a30c0fc75083f3a240895ea6726e3330
Seliverstov Maxim <maxim.web.developer@gmail.com>
2013-06-03 18:13:34 +0400

    Modify password validator.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation 
   has_secure_password 
-  validates_presence_of :password, :on => :create
+
+  validates :password_digest, presence: true
 
   has_many :owner_stories, dependent: :destroy, class_name: "Story", foreign_key: "owner_id"
   has_many :owner_comments, dependent: :destroy, class_name: "Comment", foreign_key: "owner_id"
