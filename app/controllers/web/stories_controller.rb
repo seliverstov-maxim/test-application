07c2c0f4db89cc93b35dac3ea3d0421949eae90a
Seliverstov Maxim <maxim.web.developer@gmail.com>
2013-06-03 11:36:56 +0400

    Fix uninitialize Web::StoriesController::Stroy

--- app/controllers/web/stories_controller.rb
+++ app/controllers/web/stories_controller.rb
@@ -1,25 +1,25 @@
 class Web::StoriesController < Web::ApplicationController
   def index
-    @search = Story.search(params[:q])
+    @search = ::Story.search(params[:q])
     @stories = @search.result(:distinct => true)
   end
 
   def show
-    @story = Story.find(params[:id])
+    @story = ::Story.find(params[:id])
     @comments = @story.comments.includes(:owner)
     @comment = @story.comments.build
   end
 
   def new
-    @story = Story.new
+    @story = ::Story.new
   end
 
   def edit
-    @story = Story.find(params[:id])
+    @story = ::Story.find(params[:id])
   end
 
   def create
-    @story = Story.new(params[:story])
+    @story = ::Story.new(params[:story])
 
     if @story.save
       redirect_to @story, notice: 'Story was successfully created.'
@@ -30,7 +30,7 @@ class Web::StoriesController < Web::ApplicationController
   end
 
   def update
-    @story = Story.find(params[:id])
+    @story = ::Story.find(params[:id])
 
     if @story.update_attributes(params[:story])
       redirect_to @story, notice: 'Story was successfully updated.'
@@ -40,7 +40,7 @@ class Web::StoriesController < Web::ApplicationController
   end
 
   def destroy
-    @story = Story.find(params[:id])
+    @story = ::Story.find(params[:id])
     @story.destroy
     redirect_to stories_url
   end
